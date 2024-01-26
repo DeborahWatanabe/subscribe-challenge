270cd5df957b52083fc269ca018d2497f42a7157
Deborah Watanabe <dhidemi.watanabe@gmail.com>
2024-01-26 01:57:40 -0300

    Style: fix rubocop issues

--- db/migrate/20240126000536_change_rate_to_decimal.rb
+++ db/migrate/20240126000536_change_rate_to_decimal.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class ChangeRateToDecimal < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_column :taxes, :rate, :decimal, precision: 10, scale: 6
+  end
+
+  def down
+    change_column :taxes, :rate, :float
   end
 end
